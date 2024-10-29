92f5312aba302340582f517d8cc7983cc594bb13
picarenlamina <116097495+picarenlamina@users.noreply.github.com>
2024-10-29 18:51:42 +0100

    Create esquema.sql

---
+++ sql/esquema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE usuarios (
+    id int NOT NULL AUTO_INCREMENT,
+    nombre varchar(255),
+    apellidos varchar(255)
+);
+
+insert into usuarios ( nombre, apellidos ) values ( "antonio", "ruiz" );
+insert into usuarios ( nombre, apellidos ) values ( "javier", "ruiz" );
