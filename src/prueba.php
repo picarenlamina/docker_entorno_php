d816a9a81bbf7ba1fd271e1b860e680d4c0c298c
picarenlamina <116097495+picarenlamina@users.noreply.github.com>
2024-10-29 18:47:13 +0100

    Create prueba.php

---
+++ src/prueba.php
@@ -0,0 +1,22 @@
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "test";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $stmt = $conn->prepare("SELECT * FROM usuarios");
+  $stmt->execute();
+
+  // set the resulting array to associative
+  $result = $stmt->fetchAll();
+  foreach( $result as $item ) {
+    print( "<tr><td>%s</td><td>%s</td><td>%s</td></tr>", $item["id"], $item["nombre"], $item["apellidos"]);
+  }
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+
+
+?>
